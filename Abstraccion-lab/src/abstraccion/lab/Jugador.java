2e8a6feb6b48d48e5618facb51a489e5fcbe028b
Andrés Díaz <andresalbertodiazrevelo@gmail.com>
2026-02-06 09:29:12 -0600

    Clase Jugador

---
+++ Abstraccion-lab/src/abstraccion/lab/Jugador.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abstraccion.lab;
+
+/**
+ *
+ * @author user
+ */
+public class Jugador {
+ private String Player;
+ public int puntaje; //revisar despues la diferencia de public
+ 
+ public Jugador(String Player){
+     this.Player = Player;      
+     
+     puntaje = 0; //igualmente
+ }
+ 
+ public String obtenerNombrePlayer(){
+     return Player;
+ }
+ 
+ 
+ public int mostrarPuntaje(){
+     return puntaje;
+ }
+ 
+ 
+ public int incrementarPuntaje(){
+     return puntaje++;
+ }
+ 
+ public void reiniciarPuntaje(){
+     puntaje = 0;
+ }
+ 
+ public String toString(){
+     return ("Jugador: " +Player + " Puntaje: " + puntaje);
+ }
+ 
+ 
+}
